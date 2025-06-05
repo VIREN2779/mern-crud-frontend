17a2c721cb7be995404da9cff564aa74e31e05b0
VIREN2779 <virenpatadiya073@gmail.com>
2025-06-05 12:20:12 +0530

    feat: Show component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import ShowComponents from './components/ShowComponents';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>Hello    </>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<ShowComponents />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
